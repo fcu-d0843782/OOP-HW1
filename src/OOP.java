eab9364d1df1fa4c4373d77393d890a6d60166ca
user <user@EA02>
2020-09-29 09:47:56 +0800

    last

--- src/OOP.java
+++ src/OOP.java
@@ -2,6 +2,8 @@
 public class OOP {
 	public static void main(String[] args) {
 		//TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("I'm Jason");
+		System.out.println("My nid is D0843782");
+		System.out.println("This is lab 1 hw");
 	}
 }
